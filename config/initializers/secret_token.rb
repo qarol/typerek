02cc641dd579f165bc1a2505d52cba6a7d6d90a2
Karol Bajko <karol.bajko@gmail.com>
2012-06-05 19:53:42 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Typerek::Application.config.secret_token = '8d2ce20569d618331f1149a67fabc5dd7177deff9a07014a47e48e90d684e8986129ad074c8b343c5ed456b52e692dccb5d1fa93b79f531be0c3c799f3430f02'
