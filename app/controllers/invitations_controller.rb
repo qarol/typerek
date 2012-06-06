061d9addefbcf66d8f85dfb970576ffe7ac31523
Karol Bajko <karol.bajko@gmail.com>
2012-06-06 12:53:31 +0200

    fix

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -5,6 +5,11 @@ class InvitationsController < Devise::InvitationsController
     @user = User.invite!(:username => params[:user][:username], :email => "#{params[:user][:username]}@example.com") do |u|
       u.skip_invitation = true
     end
+    if @user.valid?
+      flash.now[:notice] = "Utworzono zaporszenie"
+    else
+      flash.now[:alert] = "Wystąpił błąd, nie wygenerowano zaproszenia"
+    end
   end
 
   private
