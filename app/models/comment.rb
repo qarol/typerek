7334fe0c61e642c57fd7cec179c4d9b28eba2d2d
Karol Bajko <karol.bajko@gmail.com>
2014-05-27 02:29:32 +0200

    Poprawienie wyglądu stron

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :match
   belongs_to :user
 
+  validates_presence_of :content
+
   attr_accessible :content, :match_id, :user_id
 
   delegate :username, :to => :user, :allow_nil => true, :prefix => false
