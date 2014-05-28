e563cd1aa1cf5347d69a5f37cd282ecb613b37c9
Karol Bajko <karol.bajko@gmail.com>
2014-05-29 01:06:59 +0200

    Poprawki bezpieczeństwa

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -26,6 +26,7 @@ class MatchesController < ApplicationController
   def set_type
     @match = Match.find(params[:id])
     if @match.started?
+      flash[:alert] = "Mecz już się rozpoczął. Twój typ nie został zmieniony."
       redirect_to match_path(@match)
     else
       answer = Answer.find_by_user_id_and_match_id(current_user.id, @match.id) || @match.answers.build(:user_id => current_user.id)
