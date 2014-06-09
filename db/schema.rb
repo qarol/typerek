366b27c937cd77c67b8313f7b322f0c41ec8723e
Karol Bajko <karol.bajko@gmail.com>
2014-06-09 21:08:05 +0200

    Wykonanie migracji

--- db/schema.rb
+++ db/schema.rb
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(:version => 20120606204407) do
     t.datetime "created_at",                                              :null => false
     t.datetime "updated_at",                                              :null => false
     t.boolean  "admin",                                :default => false
-    t.string   "invitation_token", :limit => 60
+    t.string   "invitation_token",       :limit => 60
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
     t.datetime "invitation_accepted_at"
