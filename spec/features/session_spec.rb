fad1f77d25fcd91309d6e44799dcd9bba1baf722
Karol Bajko <karol@northpass.com>
2021-06-09 23:07:53 +0200

    Improve feature specs

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -3,23 +3,32 @@
 require 'rails_helper'
 
 RSpec.feature 'Session', type: :feature do
-  background do
-    create(:user, username: 'user', password: 'password')
+  feature 'When user exists and is not signed in' do
+    background do
+      create(:user, username: 'user', password: 'password')
+    end
+
+    scenario 'User signs in correctly' do
+      visit '/'
+      expect(page).not_to have_content('Cześć! Strona powstała by w gronie przyjaciół')
+      fill_in 'Login', with: 'user'
+      fill_in 'Hasło', with: 'password'
+      click_button 'Zaloguj się'
+      expect(page).to have_content('Cześć! Strona powstała by w gronie przyjaciół')
+    end
   end
 
-  scenario 'User signs in & signs out' do
-    visit '/'
+  feature 'When user exists and is already signed in' do
+    background do
+      user = create(:user, username: 'user', password: 'password')
+      login_as(user, scope: :user)
+    end
 
-    expect(page).not_to have_content('Cześć! Strona powstała by w gronie przyjaciół')
-
-    fill_in 'Login', with: 'user'
-    fill_in 'Hasło', with: 'password'
-    click_button 'Zaloguj się'
-
-    expect(page).to have_content('Cześć! Strona powstała by w gronie przyjaciół')
-
-    click_link 'user - wyloguj się'
-
-    expect(page).not_to have_content('Cześć! Strona powstała by w gronie przyjaciół')
+    scenario 'User signs out correctly' do
+      visit '/'
+      expect(page).to have_content('Cześć! Strona powstała by w gronie przyjaciół')
+      click_link 'user - wyloguj się'
+      expect(page).not_to have_content('Cześć! Strona powstała by w gronie przyjaciół')
+    end
   end
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -4,34 +4,48 @@ require 'rails_helper'
 
 RSpec.feature 'User management', type: :feature do
   feature 'User' do
-    let(:user) { create(:user) }
-
     background do
+      user = create(:user)
       login_as(user, scope: :user)
     end
 
-    scenario "cannot see 'Zaproszenia' link" do
+    scenario "doesn't have access to invitations section" do
       visit '/'
       expect(page).not_to have_link('Zaproszenia')
     end
   end
 
   feature 'Admin' do
-    let(:user) { create(:user, :admin) }
-
     background do
+      user = create(:user, :admin)
       login_as(user, scope: :user)
     end
 
-    scenario 'Admin invites user' do
+    scenario 'has to type proper username of invited user' do
       visit '/'
       click_link 'Zaproszenia'
-      expect(page).not_to have_content('custom_user')
+      fill_in 'Login', with: ''
+      click_button 'Generuj zaproszenie'
+      expect(page).to have_content('nie może być puste')
+    end
+
+    scenario 'can invite other person using proper username' do
+      visit '/'
+      click_link 'Zaproszenia'
+      within('#list-of-users') do
+        expect(page).not_to have_content('custom_user')
+      end
       fill_in 'Login', with: 'custom_user'
       click_button 'Generuj zaproszenie'
       expect(page).to have_content('Utworzono konto, link aktywacyjny:')
       click_link 'Zaproszenia'
-      expect(page).to have_content('custom_user')
+      within('#list-of-users') do
+        expect(page).to have_content('custom_user')
+      end
+    end
+
+    scenario 'can remove any person' do
+      visit '/'
     end
   end
 end
