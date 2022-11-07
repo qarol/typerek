7f9a41fdbf84ed7a74e2640a573ec69b35d18b45
Paweł Chwała <pawel.chwala@gmail.com>
2022-11-07 21:27:22 +0100

    Make migrations working

--- db/migrate/20140609190823_add_unique_index_on_answers.rb
+++ db/migrate/20140609190823_add_unique_index_on_answers.rb
@@ -1,4 +1,4 @@
-class AddUniqueIndexOnAnswers < ActiveRecord::Migration
+class AddUniqueIndexOnAnswers < ActiveRecord::Migration[4.2]
   def change
     add_index :answers, [:match_id, :user_id], :unique => true
   end
