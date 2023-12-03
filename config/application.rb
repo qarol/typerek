f67bd4d7c018cb673901fb650f59ea6d2a0a8fa8
Karol Bajko <karol.bajko@gmail.com>
2023-12-03 21:46:06 +0100

    Upgrade rails to 6.0

--- config/application.rb
+++ config/application.rb
@@ -11,14 +11,12 @@ Bundler.require(*Rails.groups)
 module Typerek
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    config.time_zone = 'Warsaw'
     config.i18n.default_locale = :pl
 
     config.autoload_paths << Rails.root.join('lib')
