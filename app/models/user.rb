4f210261fbda37bc757b03268d2d963b0520685b
Karol Bajko <karol.bajko@gmail.com>
2023-12-04 23:35:31 +0100

    Removed unused columns from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
-# Model opisuje podstawowego uzytkownika systemu
 class User < ApplicationRecord
-  self.ignored_columns += [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at,
-                           :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at,
-                           :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at,
-                           :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token,
-                           :locked_at, :invitation_token, :invitation_created_at, :invitation_sent_at,
-                           :invitation_limit]
-
   has_secure_password validations: false
 
   generates_token_for :invitation, expires_in: 24.hours do
---
+++ db/migrate/20231204222527_remove_unused_columns.rb
@@ -0,0 +1,22 @@
+class RemoveUnusedColumns < ActiveRecord::Migration[7.1]
+  def change
+    remove_column :users, :email, :string, limit: 255
+    remove_column :users, :encrypted_password, :string, limit: 255, default: ""
+    remove_column :users, :reset_password_token, :string, limit: 255
+    remove_column :users, :reset_password_sent_at, :datetime
+    remove_column :users, :remember_created_at, :datetime
+    remove_column :users, :sign_in_count, :integer, default: 0, null: false
+    remove_column :users, :current_sign_in_at, :datetime
+    remove_column :users, :last_sign_in_at, :datetime
+    remove_column :users, :current_sign_in_ip, :datetime
+    remove_column :users, :last_sign_in_ip, :datetime
+    remove_column :users, :confirmation_token, :string, limit: 255
+    remove_column :users, :confirmed_at, :datetime
+    remove_column :users, :confirmation_sent_at, :datetime
+    remove_column :users, :unconfirmed_email, :string, limit: 255
+    remove_column :users, :invitation_token, :string, limit: 255
+    remove_column :users, :invitation_created_at, :datetime
+    remove_column :users, :invitation_sent_at, :datetime
+    remove_column :users, :invitation_limit, :integer
+  end
+end
