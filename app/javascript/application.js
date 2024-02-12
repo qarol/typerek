a8b0e684da20a570ff123d8c6ecda802c8cf9b6e
Karol Bajko <karol.bajko@gmail.com>
2024-02-13 00:03:13 +0100

    Fix jQuery for bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-import "jquery"
+import "jquery";
 import "bootstrap"
-import Rails from "@rails/ujs"
+import Rails from "@rails/ujs";
 
-Rails.start()
+Rails.start();
+
+$(document).ready(function () {
+    $('.clickable-row').click(function () {
+        window.location.href = $(this).data('href');
+    });
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "jquery" # @3.7.1
 pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
+pin "jquery", to: "jquery.min.js", preload: true
