e6b4e8fdf754b5845f1ed5b279434d4f66528f91
Karol Bajko <karol.bajko@gmail.com>
2024-03-18 11:54:37 +0100

    Move JS related scripts to importmap rails

--- app/assets/javascripts/matches.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    $(function () {
-        return $('.clickable-row').click(function () {
-            return window.document.location = $(this).data('href');
-        });
-    });
-}).call(this);
