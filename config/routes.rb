f7e38f71fa796c7649468b8eb74c3235d96e7cdd
Karol Bajko <karol.bajko@gmail.com>
2024-06-04 23:15:38 +0200

    Introduce healthcheck

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
   delete 'sign_out', to: 'sessions#destroy'
