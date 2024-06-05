b2303e9d8a622ed9d8004d982371cf6ffe2322ad
Paweł Chwała <pawel.chwala@gmail.com>
2024-06-05 22:52:58 +0200

    Improve ranking sorting: sort by points, then by the username in case of equality of points.

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -3,7 +3,10 @@
 # Kontroler obsluguje podglad rankingu uzytkownikow
 class RankingsController < ApplicationController
   def show
-    @users = User.includes(answers: :match).active.sort { |a, b| b.points <=> a.points }
+    @users = User.includes(answers: :match).active.sort do |a, b|
+      points_comparison = b.points <=> a.points
+      points_comparison.zero? ? a.username <=> b.username : points_comparison
+    end
     @points = @users.map(&:points)
   end
 end
